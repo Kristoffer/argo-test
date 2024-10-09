8cd179accff6fa52bd05e9134bafb3a62c438243
Kristoffer Egefelt <kristoffer.egefelt@jppol.dk>
2024-10-09 13:36:53 +0200

    Add Astro node adapter for server output in astro.config.ts

--- helm-tutorial/tech-blog/astro.config.ts
+++ helm-tutorial/tech-blog/astro.config.ts
@@ -2,23 +2,26 @@
 import { defineConfig } from 'astro/config';
 import sitemap from '@astrojs/sitemap';
 import UnoCSS from 'unocss/astro';
+import node from "@astrojs/node";
 
+// https://astro.build/config
 export default defineConfig({
-//  image: {
-//    service: passthroughImageService()
-//  },
+  //  image: {
+  //    service: passthroughImageService()
+  //  },
   // used to generate images
-  site:
-    process.env.VERCEL_ENV === 'production'
-      ? 'https://brutal.elian.codes/'
-      : process.env.VERCEL_URL
-      ? `https://${process.env.VERCEL_URL}/`
-      : 'https://localhost:3000/',
+  site: process.env.VERCEL_ENV === 'production' ? 'https://brutal.elian.codes/' : process.env.VERCEL_URL ? `https://${process.env.VERCEL_URL}/` : 'https://localhost:3000/',
   trailingSlash: 'ignore',
-  integrations: [sitemap(), UnoCSS({ injectReset: true })],
+  integrations: [sitemap(), UnoCSS({
+    injectReset: true
+  })],
   vite: {
     optimizeDeps: {
-      exclude: ['@resvg/resvg-js'],
-    },
+      exclude: ['@resvg/resvg-js']
+    }
   },
+  output: "server",
+  adapter: node({
+    mode: "standalone"
+  })
 });
